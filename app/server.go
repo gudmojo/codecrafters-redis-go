91edbf346aed506f77b95392b57b8ae9cd4a979b
Gudmundur Johannsson <gudmundur.orn@gmail.com>
2024-11-22 21:54:42 +0000

    Respond to ECHO

--- app/server.go
+++ app/server.go
@@ -58,23 +58,38 @@ func handleConnection(conn net.Conn) {
 }
 
 func parse(buf []byte) ([]Value, error) {
-	i := 1
-	i, jmax, err := ReadNumber(buf, i)
+	log.Println("parse")
+	i := 1 // Skip *
+	log.Printf("i=%d", i)
+	i, c, err := ReadNumber(buf, i)
+	log.Printf("i=%d", i)
+	log.Println("ii", i)
 	if err != nil {
 		log.Println("Error reading number of arguments")
 		return nil, err
 	}
-	cmd := make([]Value, jmax)
-	for j := 0; j < jmax; j++ {
-		i += 3
-		i, bulkLen, err := ReadNumber(buf, i)
+	//log.Println("c", c)
+	cmd := make([]Value, c)
+	for j := 0; j < c; j++ {
+		i += 3 // Skip \r\n$
+		log.Printf("1 i=%d", i)
+		var bulkLen int
+		i, bulkLen, err = ReadNumber(buf, i)
+		log.Printf("2 i=%d", i)
+		//log.Println("bulkLen", bulkLen)
 		if err != nil {
+			log.Println("Y")
 			log.Println("Error reading bulk length")
 			return nil, err
 		}
-		i += 2
+		i += 2 // Skip \r\n
+		log.Printf("3 i=%d", i)
 		value := string(buf[i : i+bulkLen])
+		//log.Println("value", value)
 		cmd[j] = Value{typ: "bstring", str: value}
+		//log.Println("x")
+		i += bulkLen
+		log.Printf("4 i=%d", i)
 	}
 	return cmd, nil
 }
@@ -102,6 +117,7 @@ func handleCommand(cmd []Value) Value {
 }
 
 func ReadNumber(s []byte, i int) (int, int, error) {
+	log.Printf("ReadNumber %d", i)
 	j := i
 	for ; j < len(s) && unicode.IsDigit(rune(s[j])); {
 		j++
--- app/server_test.go
+++ app/server_test.go
@@ -23,20 +23,20 @@ func TestSerialize(t *testing.T) {
 }
 func TestParse(t *testing.T) {
 	tests := []struct {
-		input    []byte
+		input    string
 		expected []Value
 		err      bool
 	}{
-		{[]byte("*2\r\n$4\r\nECHO\r\n$3\r\nhey\r\n"), []Value{{typ:"bstring", str:"ECHO"}, {typ:"bstring", str:"hey"}}, false},
-		{[]byte("*1\r\n$4\r\nPING\r\n"), []Value{{typ:"bstring", str:"PING"}}, false},
-//		{[]byte("*3\r\n$3\r\nSET\r\n$3\r\nkey\r\n$5\r\nvalue\r\n"), []Value{{typ:"bstring", str:"SET"}, {typ:"bstring", str:"key"}, {typ:"bstring", str:"value"}}, false},
-//		{[]byte("*2\r\n$4\r\nECHO\r\n$-1\r\n"), nil, true},
+		{"*2\r\n$4\r\nECHO\r\n$3\r\nhey\r\n", []Value{{typ:"bstring", str:"ECHO"}, {typ:"bstring", str:"hey"}}, false},
+		{"*1\r\n$4\r\nPING\r\n", []Value{{typ:"bstring", str:"PING"}}, false},
+		{"*3\r\n$3\r\nSET\r\n$3\r\nkey\r\n$5\r\nvalue\r\n", []Value{{typ:"bstring", str:"SET"}, {typ:"bstring", str:"key"}, {typ:"bstring", str:"value"}}, false},
+		{"*2\r\n$4\r\nECHO\r\n$-1\r\n", nil, true},
 	}
 
 	for _, test := range tests {
-		output, err := parse(test.input)
+		output, err := parse([]byte(test.input))
 		if (err != nil) != test.err {
-			t.Errorf("parse(%v) error = %v; want err = %v", test.input, err, test.err)
+			t.Fatalf("parse(%v) error = %v; want err = %v", test.input, err, test.err)
 		}
 		if !test.err && !equal(output, test.expected) {
 			t.Errorf("parse(%v) = %v; want %v", test.input, output, test.expected)
@@ -48,22 +48,26 @@ func TestReadNumber(t *testing.T) {
 		input    []byte
 		start    int
 		expected int
+		expectEnd int
 		err      bool
 	}{
-		{[]byte("12345"), 0, 12345, false},
-		{[]byte("6789"), 0, 6789, false},
-		{[]byte("abc"), 0, 0, true},
-		{[]byte("123abc"), 0, 123, false},
-		{[]byte(""), 0, 0, true},
+		{[]byte("12345"), 0, 12345, 5, false},
+		{[]byte("6789"), 0, 6789, 4, false},
+		{[]byte("abc"), 0, 0, 0, true},
+		{[]byte("123abc"), 0, 123, 3, false},
+		{[]byte(""), 0, 0, 0, true},
 	}
 
 	for _, test := range tests {
-		_, output, err := ReadNumber(test.input, test.start)
+		le, output, err := ReadNumber(test.input, test.start)
 		if (err != nil) != test.err {
 			t.Errorf("ReadNumber(%v, %d) error = %v; want err = %v", test.input, test.start, err, test.err)
 		}
 		if output != test.expected {
 			t.Errorf("ReadNumber(%v, %d) = %d; want %d", test.input, test.start, output, test.expected)
+		}
+		if le != test.expectEnd {
+			t.Errorf("ReadNumber() = %d; want %d", le, test.expectEnd)
 		}
 	}
 }
