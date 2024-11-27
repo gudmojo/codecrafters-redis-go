c7ed0a6b8840c4cfb457730ccd199cbdd51aee5a
Gudmundur Johannsson <gudmundur.orn@gmail.com>
2024-11-27 20:33:05 +0000

    Remove debugging lines

--- app/rdb.go
+++ app/rdb.go
@@ -8,16 +8,16 @@ import (
 	"time"
 )
 
-type Metadata struct {
+type RdbMetadata struct {
 	attr []MetadataAttribute
 }
 
-type MetadataAttribute struct {
+type RdbMetadataAttribute struct {
 	name  string
 	value string
 }
 
-type Database struct {
+type RdbDatabase struct {
 	index         int
 	hashTableSize uint
 	expirySize    uint
@@ -51,59 +51,31 @@ func parseRDB(bytes []byte) {
 		panic("Invalid RDB version: " + rdbVersion)
 	}
 	i += 4
-	res := Metadata{}
+	res := RdbMetadata{}
 	for {
 		switch bytes[i] {
 		case 0xFA:
-			log.Printf(">>>Parsing Metadata: i=%d", i)
 			i++
 			for !isNewSection(bytes[i], i) {
-				log.Printf("ZUZUZU %x", bytes[i])
-				r := MetadataAttribute{}
-				log.Printf("Parsing MetadataKey: i=%d", i)
+				r := RdbMetadataAttribute{}
 				i, r.name = parseStringEncoded(bytes, i)
-				log.Printf("Parsed MetadataKey: %s", r.name)
-				log.Printf("Parsing MetadataValue: i=%d", i)
 				i, r.value = parseStringEncoded(bytes, i)
-				log.Printf("Parsed MetadataValue: %s", r.value)
 				res.attr = append(res.attr, r)
 			}
-			log.Printf("===Parsed Metadata: %v", res)
 		case 0xFE:
-			log.Printf(">>>Parsing DB: i=%d", i)
 			i, _ = parseDatabaseSubsection(bytes, i)
-			log.Printf("Database subsection parsed. i=%d", i)
 			for !isNewSection(bytes[i], i) {
 				i = parseObject(bytes, i)
-				log.Printf("Object parsed. i=%d", i)
 			}
-			log.Printf("===Parsed DB: i=%d", i)
 		case 0xFF:
-			parseEndOfFile(bytes, i)
+			// End of file marker
 			return
 		}
 	}
 }
 
 func isNewSection(b byte, i int) bool {
-	log.Printf("isNewSection: %x i=%d", b, i)
 	return b == 0xFF || b == 0xFE || b == 0xFA
-}
-
-func parseEndOfFile(input []byte, i int) {
-	if input[i] != 0xFF {
-		panic("Invalid end of file marker")
-	}
-/*	
-	checksumWritten := uint64(input[i+1]) | uint64(input[i+2])<<8 | uint64(input[i+3])<<16 | uint64(input[i+4])<<24 | uint64(input[i+5])<<32 | uint64(input[i+6])<<40 | uint64(input[i+7])<<48 | uint64(input[i+8])<<56
-	log.Printf("checksumWritten: %d", checksumWritten)
-	table := crc64.MakeTable(crc64.ISO)
-	checksum := crc64.Checksum(input[:i], table)
-	log.Printf("checksumCalculated: %d", checksum)
-	if checksum != checksumWritten {
-		panic("Checksum mismatch")
-	}
-		*/
 }
 
 func parseObject(input []byte, i int) int {
@@ -131,15 +103,14 @@ func parseObject(input []byte, i int) int {
 	return i
 }
 
-func parseDatabaseSubsection(input []byte, i int) (int, Database) {
+func parseDatabaseSubsection(input []byte, i int) (int, RdbDatabase) {
 	var err error
-	res := Database{}
+	res := RdbDatabase{}
 	if input[i] != 0xFE {
 		panic("Invalid database subsection header")
 	}
 	i++
 	res.index = int(input[i])
-	log.Printf("Parsed Database index: %d", res.index)
 	i++
 	if input[i] != 0xFB {
 		panic("Invalid marker for hash table size information follows")
@@ -153,7 +124,6 @@ func parseDatabaseSubsection(input []byte, i int) (int, Database) {
 	if err != nil {
 		panic(err)
 	}
-	log.Printf("Parsed Database: %v", res)
 	return i, res
 }
 
@@ -166,14 +136,12 @@ func parseSizeEncoded(input []byte, i int) (int, uint, string, error) {
 	// The size is the remaining 6 bits of the byte.
 	if input[i]>>6 == 0 {
 		x := uint(input[i])
-		log.Printf("Parsed size encoded 1: %d i=%d", x, i)
 		return i+1, x, "", nil
 	}
 	// If the first two bits are 0b01:
 	// The size is the next 14 bits
 	if input[i]>>6 == 1 {
 		x := uint(input[i]&0x3F)<<8 | uint(input[i+1])
-		log.Printf("Parsed size encoded 2: %d", x)
 		return i + 2, x, "", nil
 	}
 	// If the first two bits are 0b10:
@@ -181,7 +149,6 @@ func parseSizeEncoded(input []byte, i int) (int, uint, string, error) {
 	// The size is the next 4 bytes, in big-endian (read left-to-right).
 	if input[i]>>6 == 2 {
 		x := uint(input[i+1])<<24 | uint(input[i+2])<<16 | uint(input[i+3])<<8 | uint(input[i+4])
-		log.Printf("Parsed size encoded 3: %d", x)
 		return i + 5, x, "", nil
 	}
 	if input[i]>>6 == 3 {
@@ -192,7 +159,6 @@ func parseSizeEncoded(input []byte, i int) (int, uint, string, error) {
 			i++
 			x := fmt.Sprintf("%d", input[i])
 			i++
-			log.Printf("Parsed size encoded 4: %s", x)
 			return i, 0, x, nil
 		} else if input[i] == 0xC1 {
 			// The 0xC1 size indicates the string is a 16-bit integer.
@@ -201,7 +167,6 @@ func parseSizeEncoded(input []byte, i int) (int, uint, string, error) {
 			// C1 39 30
 			d := uint(input[i+1]) | uint(input[i+2])<<8
 			i += 3
-			log.Printf("Parsed size encoded 5: %d", d)
 			return i, 0, fmt.Sprintf("%d", d), nil
 		} else if input[i] == 0xC2 {
 			// The 0xC2 size indicates the string is a 32-bit integer.
@@ -210,7 +175,6 @@ func parseSizeEncoded(input []byte, i int) (int, uint, string, error) {
 			// C2 87 D6 12 00
 			d := uint(input[i+1]) | uint(input[i+2])<<8 | uint(input[i+3])<<16 | uint(input[i+4])<<24
 			i += 5
-			log.Printf("Parsed size encoded 6: %d", d)
 			return i, 0, fmt.Sprintf("%d", d), nil
 		} else if input[i] == 0xC3 {
 	        // The 0xC3 size indicates that the string is compressed with the LZF algorithm.
@@ -229,7 +193,6 @@ func parseStringEncoded(input []byte, i int) (int, string) {
 	}
 	// Special case for compressed strings when the size is between 0xC0-0xC3
 	if str != "" {
-		log.Printf("Parsed string encoded: %s", str)
 		return i, str
 	}
 	// Normal strings
