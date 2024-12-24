b5d4313d861e08ef4e5b9cf784f503d57b1c275f
Gudmundur Johannsson <gudmundur.orn@gmail.com>
2024-12-24 10:13:57 +0000

    Executing a transaction

--- app/commands.go
+++ app/commands.go
@@ -173,12 +173,13 @@ func execCommand(req *Value, session *Session) Value {
 	if session.Transaction == nil {
 		return Value{Typ: "error", Str: "ERR EXEC without MULTI"}
 	}
+	tr := session.Transaction
+	session.Transaction = nil
 	res := Value{Typ: "array", Arr: []Value{}}
-	for _, cmd := range session.Transaction.Commands {
+	for _, cmd := range tr.Commands {
 		r := HandleRequest(cmd, 0, session)
 		res.Arr = append(res.Arr, r)
 	}
-	session.Transaction = nil
 	return res
 }
 
